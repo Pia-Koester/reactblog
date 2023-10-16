632356eea3a6b268c75113476ebd1c24f4f1ab4a
Pia <pia.koester@hey.com>
2023-10-16 17:21:49 +0200

    feat: create initial form with input areas

---
+++ src/components/Form.jsx
@@ -0,0 +1,12 @@
+export default function Form() {
+    
+    return (
+        <form>
+        <input type="text" placeholder = "Your Blog Title" />
+        <input type="text" placeholder = "Your Image" />
+        <input type="text" placeholder = "Your Description" />
+        <input type="date"  />
+        </form>
+
+    )
+}
