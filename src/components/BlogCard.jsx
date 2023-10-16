5e8177041bbea41303aa11f38162c83055d58c69
Pia <pia.koester@hey.com>
2023-10-16 18:38:41 +0200

    add Blogcards and form

---
+++ src/components/BlogCard.jsx
@@ -0,0 +1,17 @@
+export default function BlogCard() {
+    return(
+        <div class="card">
+            <img
+            //   src=
+            //   alt=
+              className="card__img"
+            />
+            <div className="card__content">
+              <h2 className="card__title">Blog Post</h2>
+              <p className="card__description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure, tempora.</p>
+              <p className="card__date">date</p>
+            </div>
+          </div>
+    )
+
+}
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,19 @@
+import BlogCard from "./BlogCard";
+
+// create the blog array with objects for each item 
+// use useState to store the information
+
 export default function Form() {
     
     return (
-        <form>
-        <input type="text" placeholder = "Your Blog Title" />
-        <input type="text" placeholder = "Your Image" />
-        <input type="text" placeholder = "Your Description" />
-        <input type="date"  />
-        </form>
-
+        <div>
+         <form>
+            <input type="text" placeholder = "Your Blog Title" />
+            <input type="text" placeholder = "Your Image" />
+            <input type="text" placeholder = "Your Description" />
+            <input type="date"  />
+            </form>
+        <BlogCard/> /* once there is the blog array then use the map function to create each card */
+        </div>
     )
 }
