cb7b2af9dfca1bfb69501db96c6d65149513c042
Pia <pia.koester@hey.com>
2023-10-18 12:36:31 +0200

    import MUI for styling and icons

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,17 +1,75 @@
-export default function BlogCard() {
-    return(
-        <div class="card">
-            <img
-            //   src=
-            //   alt=
-              className="card__img"
-            />
-            <div className="card__content">
-              <h2 className="card__title">Blog Post</h2>
-              <p className="card__description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure, tempora.</p>
-              <p className="card__date">date</p>
-            </div>
-          </div>
-    )
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+export default function BlogCard() {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+ 
+      <CardMedia
+        component="img"
+        height="194"
+        image="" // image url which should come from props
+        alt="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          Blog Description text from props 
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon /> {/*create a counter next to it */}
+        </IconButton>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Comments:</Typography>
+          <Typography paragraph>
+           comments  {/*add the comments here*/}
+          </Typography>
+         
+  
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,19 +1,31 @@
-import BlogCard from "./BlogCard";
+
+import { useState } from "react";
+import TextField from '@mui/material/TextField';
+import { Button } from "@mui/material";
+import BlogCard from "./BlogCard"
+
 
 // create the blog array with objects for each item 
 // use useState to store the information
 
 export default function Form() {
-    
+   const [blogs, setBlogs] = useState([{id: 1, title : "Test", description: "Test TEst", image: "img"}]);
+
     return (
         <div>
-         <form>
-            <input type="text" placeholder = "Your Blog Title" />
-            <input type="text" placeholder = "Your Image" />
-            <input type="text" placeholder = "Your Description" />
-            <input type="date"  />
+         <form>        
+            <TextField id="outlined" label="Title" variant="outlined" type="text" placeholder = "Your Title"/>
+         <TextField id="outlined-basic" label="Description" variant="outlined" type="text" placeholder = "Your Description"/>
+         <TextField id="outlined-basic" label="Image" variant="outlined" type="text" placeholder = "Your Image"/>
+            <Button variant="outlined" onClick={(e) =>{ console.log("Testnachricht")}}>Submit</Button>
+        
             </form>
-        <BlogCard/> /* once there is the blog array then use the map function to create each card */
+            <BlogCard/>
+            <BlogCard/>
+            <BlogCard/>
+            <BlogCard/>
+         
+            
         </div>
     )
 }
